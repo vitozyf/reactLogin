fba4e42f4a87b3b64a72d5365822fa4eac5ec158
vito.z <vito.z@qq.com>
2018-06-14 00:23:21 +0800

    router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import NavigationBar from './components/NavigationBar'
 import routes from './routers'
