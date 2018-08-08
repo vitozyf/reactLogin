d8e73a30de966097eace59487781b6d979b6d7c0
vito.z <vito.z@qq.com>
2018-08-08 21:00:23 +0800

    生产环境增加initsql

--- server/init-sql.js
+++ server/init-sql.js
@@ -1,10 +1,10 @@
 import * as models from './src/model/model-sequelize/index.js';
 // const ISPRODUCTION = process.argv[2] === 'production'
-console.log(123, models)
 for (const key in models) {
   if (models.hasOwnProperty(key)) {
     const model = models[key];
-    model.sync({force: true}).then(res => {
+    // {force: true}
+    model.sync().then(res => {
       console.log('sync data success', process.env.NODE_ENV)
     }).catch(err => {
       console.log('sync data fail', process.env.NODE_ENV)
--- server/src/index.js
+++ server/src/index.js
@@ -12,15 +12,19 @@ const ISPRODUCTION =  process.env.NODE_ENV === 'production'
 const PORT = Config[ISPRODUCTION ? 'Production' : 'Dev'].port
 let app = express();
 
+if (ISPRODUCTION) {
+  require('../init-sql');
+}
+
 app = logInit(app); // 日志
 app.all('*', AccessControl);
 app.use(express.static('public')); // 静态资源
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(sendJson); // 封装res自定义方法
 app.use(session);
 app.use(SessionValidation); // 全局登录验证
 app.use(userRemember); // 用户是否勾选remeber
-app.use(sendJson); // 封装res自定义方法
 
 createRouter(app)
 // console.log(process.env.NODE_ENV)
