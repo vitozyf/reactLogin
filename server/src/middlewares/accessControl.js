933bc5a2e38ab0d31fa3cb8177cea1a4c1ef1fbb
zhangyongfeng <535069215@qq.com>
2018-08-09 17:49:42 +0800

    log Origin

--- server/src/middlewares/accessControl.js
+++ server/src/middlewares/accessControl.js
@@ -9,7 +9,7 @@ logger.level = 'debug';
 export default function (req, res, next) {
   // 设置响应头，允许跨域
   if(crossDomainWhiteList.indexOf(req.hostname) > -1){
-    logger.debug('headerinfo', req.hostname, req.headers.origin, crossDomainWhiteList);
+    logger.debug('headerinfo', req, req.hostname, req.headers.origin, crossDomainWhiteList);
     res.header('Access-Control-Allow-Origin', req.headers.origin); // 跨域白名单
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
