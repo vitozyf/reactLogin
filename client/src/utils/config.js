6aa748f2155b75bc5471cc65b124700a66d8fa58
zhangyongfeng <535069215@qq.com>
2018-08-13 18:55:48 +0800

    本地开启https

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -2,5 +2,5 @@ const DEBUG = process.env.NODE_ENV !== 'production'
 
 export default {
   SessionIdName: 'JsTechnicalForum',
-  BaseUrl: DEBUG ? 'http://localhost:6060/' : 'https://www.vitock.cn:6060/'
+  BaseUrl: DEBUG ? 'https://localhost:6060/' : 'https://www.vitock.cn:6060/'
 }
